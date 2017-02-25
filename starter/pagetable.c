76f3ea027dd1c1e1dca7c71fb9668a348f25c966
sumand <sumand@ca.ibm.com>
2017-02-25 18:50:11 -0500

    fixed“

--- starter/pagetable.c
+++ starter/pagetable.c
@@ -162,30 +162,21 @@ char *find_physpage(addr_t vaddr, char type) {
 	if(!(p->frame & PG_VALID)){
 
 		miss_count++;
-		printf("into valid\n");
 		if(!(p->frame & PG_ONSWAP)){
-			printf("1\n");
 			int frame_num = allocate_frame(p);
 			init_frame(frame_num,vaddr);
 			p->frame = frame_num << PAGE_SHIFT;
 
 		}
 		else{
-			printf("2\n");
 			int frame_num = allocate_frame(p);
 			swap_pagein(frame_num,p->swap_off);
-			
 			p->frame = frame_num << PAGE_SHIFT;
-			// p->swap_off = INVALID_SWAP;
 			p->frame = p->frame & ~PG_ONSWAP;
 			p->frame = p->frame & ~PG_DIRTY;
-
-
 		}
-
 	}
 	else{
-		printf("into normal\n");
 		hit_count++;
 	}
 	
